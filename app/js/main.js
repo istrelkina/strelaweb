882fa753b5e43bbbab5e19abf2ff69a3b953cc47
istrelkina <istrelkina@gmail.com>
2021-04-16 15:25:12 +1000

    added adaptive version

---
+++ app/js/main.js
@@ -0,0 +1,19 @@
+$(function(){
+
+
+$('.order-btn').on('click', function() {
+
+	    let href = $(this).attr('href');
+
+	    $('html, body').animate({
+	        scrollTop: $(href).offset().top
+	    }, {
+	        duration: 470,   // по умолчанию «400»
+	        easing: "linear" // по умолчанию «swing»
+	    });
+	     
+	    return false;
+	});
+
+
+});
